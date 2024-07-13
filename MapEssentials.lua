b06f4f4bc7d2ee1fb487eae162c3aefd399c2840
CelestialDodo <120543993+CelestialDodo@users.noreply.github.com>
2024-07-13 10:25:15 -0700

    Create MapEssentials.lua

---
+++ MapEssentials.lua
@@ -0,0 +1,28 @@
+
+local MapEssentials = {} 
+
+MapEssentials.Map = nil
+MapEssentials.CurrentMap = nil
+MapEssentials.Debounce = false 
+
+function MapEssentials.PlaceMap(Location)
+    If MapEssentials.Debounce then
+        return
+    else
+        MapEssentials.Debounce = true
+    end
+    If MapEssentials.CurrentMap then
+        MapEssentials.CurrentMap:destroy()
+    end
+    local map = MapEssentials.Map:Clone()
+    map:PivotTo(Location.CFrame)
+    MapEssentials.CurrentMap  = map
+    MapEssentials.Debounce = false
+end 
+
+function MapEssentials.RandomMap(Maps)
+    Maps = Maps:GetChildren()
+    MapEssentials.Map = Maps[math.random(1,#Maps)]
+end 
+
+return MapEssentials
