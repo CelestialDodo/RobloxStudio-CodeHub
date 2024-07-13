773c80fd47691eafdd3558cd435e513b6c3e4f3b
CelestialDodo <120543993+CelestialDodo@users.noreply.github.com>
2024-07-13 11:01:04 -0700

    Update MapEssentials.lua

--- MapEssentials.lua
+++ MapEssentials.lua
@@ -1,28 +1,33 @@
--- THIS CODE IS IN TESTING, DO NOT EXPECT IT ALL TO WORK
-local MapEssentials = {} 
+
+local MapEssentials = {}
 
 MapEssentials.Map = nil
 MapEssentials.CurrentMap = nil
-MapEssentials.Debounce = false 
-
-function MapEssentials.PlaceMap(Location)
-    If MapEssentials.Debounce then
-        return
-    else
-        MapEssentials.Debounce = true
-    end
-    If MapEssentials.CurrentMap then
-        MapEssentials.CurrentMap:destroy()
-    end
-    local map = MapEssentials.Map:Clone()
-    map:PivotTo(Location.CFrame)
-    MapEssentials.CurrentMap  = map
-    MapEssentials.Debounce = false
-end 
+MapEssentials.Debounce = false
 
 function MapEssentials.RandomMap(Maps)
-    Maps = Maps:GetChildren()
-    MapEssentials.Map = Maps[math.random(1,#Maps)]
-end 
+	Maps = Maps:GetChildren()
+	MapEssentials.Map = Maps[math.random(1,#Maps)]
+end
+
+function MapEssentials.PlaceMap(Location, MapParent)
+	if not MapParent then
+		MapParent = Location
+	end
+	if MapEssentials.Debounce then
+		return
+	else
+		MapEssentials.Debounce = true
+	end
+	if MapEssentials.CurrentMap then
+		MapEssentials.CurrentMap:destroy()
+	end
+	local map = MapEssentials.Map:Clone()
+	map:PivotTo(Location.CFrame)
+	map.Name = "Map"
+	map.Parent = MapParent
+	MapEssentials.CurrentMap = map
+	MapEssentials.Debounce = false
+end
 
 return MapEssentials
