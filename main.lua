2b87f6bf17adc66fcd5caef3780244f5f43dd5c1
CelestialDodo <120543993+CelestialDodo@users.noreply.github.com>
2024-07-13 14:26:18 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,27 +1,27 @@
 local Item = {}
 
 function Item.GetCode(FileName, StoreModules)
-  if StoreModules == nil then StoreModules = false end
-  FileName = string.gsub(FileName,".lua","")
-  File = "https://raw.githubusercontent.com/CelestialDodo/RobloxGameCodeTemplates/main/"..FileName..".lua"
-  Module = loadstring(game:GetService("HttpService"):GetAsync(File))()
-  CodeHubScripts = require(18476370903)
-  if StoreModules then
-    print("STORING MODULE...")
-    Modules = game:GetService("ServerScriptService").CodeHub:FindFirstChild("Modules")
-    if not Modules then Modules = CodeHubScripts.Modules:Clone() Modules.Parent = game:GetService("ServerScriptService").CodeHub end
-    temp = {
-      [tostring(FileName)] = "Test"
-    }
-    print(temp)
-    table.insert(require(Modules), temp)
-    print(require(Modules))
-  end
-  return Module, CodeHubScripts
+	if StoreModules == nil then StoreModules = false end
+	FileName = string.gsub(FileName,".lua","")
+	local File = "https://raw.githubusercontent.com/CelestialDodo/RobloxGameCodeTemplates/main/"..FileName..".lua"
+	local Module = loadstring(game:GetService("HttpService"):GetAsync(File))()
+	local CodeHubScripts = require(18476370903)
+	if StoreModules then
+		print("STORING MODULE...")
+		local Modules = game:GetService("ServerScriptService").CodeHub:FindFirstChild("Modules")
+		if not Modules then Modules = CodeHubScripts.Modules:Clone() Modules.Parent = game:GetService("ServerScriptService").CodeHub end
+		local temp = {
+			[tostring(FileName)] = "Test"
+		}
+		print(temp)
+		table.insert(require(Modules), temp)
+		print(require(Modules))
+	end
+	return Module, CodeHubScripts
 end
 
 function Item.test()
-  print("This test works!")
+	print("This test works!")
 end
 
 return Item
