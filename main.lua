53c5e3c3a084ccd5af538547e98d06fdb3e39eff
CelestialDodo <120543993+CelestialDodo@users.noreply.github.com>
2024-07-13 14:04:50 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,8 +1,14 @@
 local Item = {}
 
-function Item.GetCode(FileName)
+function Item.GetCode(FileName, StoreModules)
   FileName = "https://raw.githubusercontent.com/CelestialDodo/RobloxGameCodeTemplates/main/"..string.gsub(FileName,".lua","")..".lua"
-  return loadstring(game:GetService("HttpService"):GetAsync(FileName))(), require(18476370903)
+  Module = loadstring(game:GetService("HttpService"):GetAsync(FileName))()
+  if StoreModules then
+    local Modules = script:FindFirstChild("Modules")
+    if not Modules then Modules = CodeHubScripts.Modules:Clone() Modules.Parent = script end
+    table.insert(Modules, require(Module))
+  end
+  return Module, require(18476370903)
 end
 
 function Item.test()
